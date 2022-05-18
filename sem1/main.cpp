98ab0a069cf85b83255a73739c293e30013eea5e
linux <noeliapantigosop@gmail.com>
2022-05-18 17:25:33 -0500

    dos

--- sem1/main.cpp
+++
@@ -1,9 +0,0 @@
-//PROYECTO: Tem libre, debe incluir árboles (cualquier tipo, estrella, búsqueda, binarios,) o grafos
-//Toma de desiciones: para juego de ajedrez / escoger la mejor ruta
-
-#include <iostream>
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
